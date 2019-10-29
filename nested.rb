726b3da19df7c1fe933efca5d6082623169163c9
Tavis Weeden <tavisweeden@gmail.com>
2019-10-29 02:24:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages.insert(1, "Assembly")]  
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
